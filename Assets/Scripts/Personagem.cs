c12bda4f36052774bd7984fe3a8b3563f79ad92d
Murilo Formiga <murilo_almeida@live.com>
2017-07-07 01:00:23 -0300

    Fire in the hole

--- Assets/Scripts/Personagem.cs
+++ Assets/Scripts/Personagem.cs
@@ -4,34 +4,9 @@ using UnityEngine;
 
 public class Personagem : MonoBehaviour {
 
-	//Variaveis
-	string nomePersonagem, nomeAtqEspecial, classePersonagem;
-	int danoAtqConvencional, vidaPersonagem; 
-
-	//Metodos GET e SET
-	public string nomePersonagem{
-		get { return nomePersonagem; }
-		set { nomePersonagem = value; }
-	}
-
-	public string nomeAtqEspecial{
-		get { return nomeAtqEspecial; }
-		set { nomeAtqEspecial = value; }
-	}
-
-	public string classePersonagem {
-		get { return classePersonagem; }
-		set { classePersonagem = value; }
-	}
-		
-	public int danoAtqConvencional{
-		get { return danoAtqConvencional; }
-		set { classePersonagem = value; }
-	}
-
-	public int vidaPersonagem{
-		get { return vidaPersonagem; }
-		set{ vidaPersonagem = value; } 
-	}
-
+	public string nome;
+	public string especial;
+	public string classe;
+	public int ataque;
+	public int vida;
 }
