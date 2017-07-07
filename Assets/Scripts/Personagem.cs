3f900261a1f38f23cbf4857186230892d3b43f52
Pablo Rodrigo <pablorodrigo552@gmail.com>
2017-07-07 00:19:33 -0300

    agora vai

---
+++ Assets/Scripts/Personagem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour {
+
+	//Variaveis
+	string nomePersonagem, nomeAtqEspecial, classePersonagem;
+	int danoAtqConvencional, vidaPersonagem; 
+
+	//Metodos GET e SET
+	public string nomePersonagem{
+		get { return nomePersonagem; }
+		set { nomePersonagem = value; }
+	}
+
+	public string nomeAtqEspecial{
+		get { return nomeAtqEspecial; }
+		set { nomeAtqEspecial = value; }
+	}
+
+	public string classePersonagem {
+		get { return classePersonagem; }
+		set { classePersonagem = value; }
+	}
+		
+	public int danoAtqConvencional{
+		get { return danoAtqConvencional; }
+		set { classePersonagem = value; }
+	}
+
+	public int vidaPersonagem{
+		get { return vidaPersonagem; }
+		set{ vidaPersonagem = value; } 
+	}
+
+}
